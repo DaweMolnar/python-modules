f83e6b5bff9cea195cca9283a1504267b3238631
David Molnar <dawemolnar@gmail.com>
2016-01-24 00:01:51 +0100

    Watchdog pylint fix

--- util/Watchdog.py
+++ util/Watchdog.py
@@ -2,20 +2,20 @@
 """Watchdog module"""
 from threading import Timer
 
-class Watchdog:
+class Watchdog(object):
     """Watchdog class. Usage:
     Watchdog([timeout],[function to call on timeout])"""
-    def __init__(self,  timeout,  user_handler=None):  # timeout in seconds
+    def __init__(self, timeout, user_handler=None):  # timeout in seconds
         self.timeout = timeout
         self.handler = user_handler if user_handler is not None \
 		    else self.default_handler
-        self.timer = Timer(self.timeout,  self.handler)
+        self.timer = Timer(self.timeout, self.handler)
         self.timer.start()
 
     def reset(self):
         """Resets the watchdog to restart the timer"""
         self.timer.cancel()
-        self.timer = Timer(self.timeout,  self.handler)
+        self.timer = Timer(self.timeout, self.handler)
         self.timer.start()
 
     def stop(self):
